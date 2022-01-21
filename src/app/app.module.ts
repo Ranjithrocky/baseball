122df1ae05e9d567be6dfd4f54b0871e81a948fa
Ranjith <2k18cse082@kiot.ac.in>
2022-01-21 16:41:30 +0530

    signup page 1st commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import {  FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -10,7 +11,10 @@ import { SignupComponent } from './signup/signup.component';
     SignupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  SignUp!: FormGroup;
+  user=['user','admin'];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.SignUp= new FormGroup({
+      "users": new FormControl(null,Validators.required),
+      "email": new FormControl(null,[Validators.required,Validators.email]),
+      "username": new FormControl(null,[Validators.required,Validators.pattern("[a-zA-Z0-9_]{8,12}")]),
+      "mobile": new FormControl(null,[Validators.required,Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]),
+      "password": new FormControl(null,Validators.required),
+      "confirmpass": new FormControl(null,[Validators.minLength(8),Validators.maxLength(16)])
+    })
   }
 
 }
